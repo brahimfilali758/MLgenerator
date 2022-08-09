c73961241bfbf9aca9ed00147f8af780dd9d1b43
ibrahim filali <brahimfilali758@gmail.com>
2022-08-09 14:47:06 +0000

    adding training data

--- data_prep.py
+++ data_prep.py
@@ -1,1 +1,16 @@
 import pandas
+import cv2
+import numpy
+import os
+from glob import glob
+#==============
+print("data_prep file executed")
+#==============
+
+data_folder = "data"
+os.chdir(data_folder)
+print(f"the current folder is : {os.getcwd()}")
+
+
+
+
