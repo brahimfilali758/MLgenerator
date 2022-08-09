b3f96500ab6b75177627d4083a2bce0d06d9f3a2
ibrahim filali <brahimfilali758@gmail.com>
2022-08-09 15:39:40 +0000

    visualise a random pokemon

--- data_prep.py
+++ data_prep.py
@@ -3,14 +3,23 @@ import cv2
 import numpy
 import os
 from glob import glob
+import matplotlib.pyplot as plt
+from os.path import join
+import random
 #==============
 print("data_prep file executed")
 #==============
 
 data_folder = "data"
-os.chdir(data_folder)
 print(f"the current folder is : {os.getcwd()}")
 
+files_list = os.listdir(data_folder)
+print(f"the first file is : \n {files_list[0]}")
 
+def get_rand_pok():
+    files_list = os.listdir(data_folder)
+    filee = random.choice(files_list)
+    im_arry = cv2.imread(join(data_folder,filee))
+    return im_arry
 
 
